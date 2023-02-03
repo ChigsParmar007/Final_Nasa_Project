fb943e5a12d9b2511585ba5c1eac98ed3e009cfc
Chirag_parmar <parmar.chirag.dcs23@vnsgu.ac.in>
2023-02-03 20:08:48 +0530

    Initial commit

---
+++ server/src/services/mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+// Update below to match your own MongoDB connection string.
+// const MONGO_URL = process.env.MONGO_URL
+
+const MONGO_URL='mongodb+srv://nasa-api:Chigs6779@nasacluster.gbhw11r.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.set('strictQuery', false)
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!')
+})
+
+mongoose.connection.on('error', (err) => {
+  console.error(err)
+})
+
+const mongoConnect = async () => {
+  await mongoose.connect(MONGO_URL)
+}
+
+const mongoDisconnect = async () => {
+  await mongoose.disconnect()
+}
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect
+}
