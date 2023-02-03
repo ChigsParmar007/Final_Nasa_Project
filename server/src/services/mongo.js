1016d05fe8c8954d8a1ef1c2a466f5777270b3ae
Chirag_parmar <parmar.chirag.dcs23@vnsgu.ac.in>
2023-02-03 20:15:05 +0530

    Some changes to run this project when it upload

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 // Update below to match your own MongoDB connection string.
 // const MONGO_URL = process.env.MONGO_URL
 
-const MONGO_URL='mongodb+srv://nasa-api:Chigs6779@nasacluster.gbhw11r.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URL = 'mongodb+srv://nasa-api:Chigs6779@nasacluster.gbhw11r.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.set('strictQuery', false)
 
@@ -18,7 +18,7 @@ mongoose.connection.on('error', (err) => {
 })
 
 const mongoConnect = async () => {
-  await mongoose.connect(MONGO_URL)
+  await mongoose.connect('mongodb://127.0.0.1:27017/NasaData')
 }
 
 const mongoDisconnect = async () => {
